db8305a601aea7e38cf4bdd176c56b55a6257703
aatitkarki <aatitkarki123@gmail.com>
2023-07-19 21:36:59 +0545

    added config files

--- customCommands/commands.sh
+++ customCommands/commands.sh
@@ -1,13 +1,30 @@
+#UTILITY COMMANDS
+function myip(){
+    ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'
+}
 #DOCKER COMMANDS
 
 # Usage dclog serviceName (dclog mobile-api) 
 function dclog() { 
+    printCommand "docker-compose up $1 -d && docker-compose logs -f $1"
     docker-compose up $1 -d && docker-compose logs -f $1
+}
+
+# Usage dcrun command (dcrun mobile-api yarn install)
+function dcrun() {
+    printCommand "docker-compose run --no-deps "${@}""
+  docker-compose run --no-deps "${@}"
 }
 
 # Usage dcdown serviceName (dcdown mobile-api)
 function dcdown() {
+    printCommand "docker-compose down $1"
      docker-compose down $1
+}
+
+function printCommand(){
+    local command=$1
+    echo "Running command: $command "
 }
 
 
